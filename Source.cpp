ec4a854392dd73e9e194276d37b347a166c8489c
Vedran Knezevic <vedranknezevic1@yahoo.com>
2018-03-22 20:16:39 +0100

    source

---
+++ Source.cpp
@@ -0,0 +1,30 @@
+#include <SFML/Graphics.hpp>
+#include "Cvijet.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(400, 400), "Hello, SFML world!");
+	window.setFramerateLimit(60);
+	Cvijet cvijet(&window);
+
+	while (window.isOpen())
+	{
+		
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		
+		cvijet.draw();
+
+		window.display();
+	}
+	
+	
+	return 0;
+}
